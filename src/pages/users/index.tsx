59db33d448cb8319f9b1d0565ee5fed371672581
Arthur Marques <arthur.l2@hotmail.com>
2022-02-09 22:37:57 -0300

    Users page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,76 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6" >
+                <Sidebar />
+
+                <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontweight="normal">Usuários</Heading>
+
+                        <Button 
+                            as="a" 
+                            size="sm" 
+                            fontSize="sm" 
+                            colorScheme="pink" 
+                            leftIcon={<Icon as={RiAddLine} fontSize="16" />}
+                        >
+                            New user
+                        </Button>
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink"></Checkbox>
+                                </Th>
+                                <Th>
+                                    User
+                                </Th>
+                                <Th>
+                                    Registration Date
+                                </Th>
+                                <Th w="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink"></Checkbox>
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold" >Arthur Marques</Text>
+                                        <Text fontSize="sm" color="gray.300">Arthur.L2@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    01 de Janeiro, 2022
+                                </Td>
+                                <Td>
+                                    <Button 
+                                        as="a" 
+                                        size="sm" 
+                                        fontSize="sm" 
+                                        colorScheme="purple" 
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Edit
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+            </Flex>
+        </Box>
+    );
+}
